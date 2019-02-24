dd1da67ee5bb5616e248652e01b375a00ad06b83
Gildardo Orozco <gorozco@Gildardos-Mac.local>
2019-02-23 16:57:45 -0800

    Favorite/unfavorite tweets done

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -15,6 +15,43 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!    
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var reTweetButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if(toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorited(true)
+            }, failure: { (error) in
+                print("Favorited did not suceed: \(error)")
+            })
+        }else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorited(false)
+            }, failure: { (error) in
+                print("unFavorited did not succed: \(error)")
+            })
+        }
+    }
+    
+    @IBAction func reTweet(_ sender: Any) {
+        
+    }
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    func setFavorited(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }else{
+            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
